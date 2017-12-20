48ffae9887dc24108bdd98922648212944f77f81
Tymm Schmitke <ifellinaholeonce@gmail.com>
2017-12-20 22:49:35 +0000

    cleaned up code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,18 +3,20 @@ function initMap() {
 
   $.ajax({
     method: "GET",
-    url: "/api/maps"
+    url: "/api/markers" //TODO change the call to /api/:id(map)/markers so that we only get the relevant markers
   }).done((maps) => {
 
-    var toronto = {lat:43, lng:-79};
-
+    //DRAW THE GOOGLE MAP
     gmap = new google.maps.Map(document.getElementById('map'), {
-      center: {lat:0, lng: 0},
+      center: {lat:0, lng: 0}, //TODO make a relevant center depending on the map
       zoom: 8
     });
 
+    //Looks at all of the markers from the map DB
     for(let point of maps) {
+      //Gets the longitude and latitude of the current marker
       let markerDot = {lat:Number(point.latitude), lng:Number(point.longitude)};
+      //Draws the marker on the map
       let marker = new google.maps.Marker({
         position: markerDot,
         map: gmap
